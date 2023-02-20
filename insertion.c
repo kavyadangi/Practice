29b1cc534a16156d1a9d54b6dd0a654f032f7113
Kavya <kavyadangi09@gmail.com>
2023-02-20 17:05:00 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -7,12 +7,23 @@ int main(){
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("before insertion : ");
+    printf("before insertion : \n");
     for(i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
-    printf("khaa krana h insertion");
+    printf("\nkhaa krana h insertion\n");
     scanf("%d",&pos);
-    temp=n;
-    
+    temp=n-1;
+    n=n+1;
+    while(temp>=pos){
+        arr[temp+1]=arr[temp];
+        temp--;
+    }
+    printf("ch; bta qa dlna h\n ");
+    scanf("%d",&arr[pos]);
+    printf("afeter insertion\n");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+return 0;
 }
