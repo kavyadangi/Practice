f4a40f4d45a8bdf563bacf0049b32ed125251abc
Kavya <kavyadangi09@gmail.com>
2023-02-20 16:41:00 +0530

    Create day2.c

---
+++ day2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int n,i,pos;
+    printf("eh enter kr zsixe wrna mrungi");
+    scanf("%d",&n);
+    int arr[n];
+    for( i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Before insertion aray is: ");
+    for( i=0;i<n;i++){
+    printf("%d ",arr[i]);
+    }
+    printf("h be bta kidhr dlna h ");
+    scanf("%d",&pos);
+    int temp;
+    temp=n;
+    while(temp>=pos){
+        arr[temp+1]=arr[temp];
+        temp=temp-1;
+    }
+    n=n+1;
+    printf("ha  btaa samaksh bta value de\n");
+    scanf("%d",&arr[pos]);
+    printf("after insertend + bht saari ddaaat + bnane k bd\n");
+    for( i=0;i<n;i++){
+    printf("%d ",arr[i]);
+    }
+    
+ return 0;          
+}
