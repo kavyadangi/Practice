ef463b1c75a34b68f6a0af070b1108bb68175c9e
Kavya <kavyadangi09@gmail.com>
2023-02-20 16:55:09 +0530

    Create insertion.c

---
+++ insertion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i,pos,temp;
+    printf("enter no of elemnts");
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("before insertion : ");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("khaa krana h insertion");
+    scanf("%d",&pos);
+    temp=n;
+    
+}
