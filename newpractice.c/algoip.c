bd60b05e71468f30ef02824a6500deba1acb2d7b
Kavya <kavyadangi09@gmail.com>
2023-04-26 22:23:15 +0530

    Create algoip.c

---
+++ newpractice.c/algoip.c
@@ -0,0 +1,14 @@
+Step 1: Scan each character in the infix
+expression. For this, repeat Steps
+2-8 until the end of infix expression
+Step 2: Push the operator into the operator
+operand into the operand stack,
+all the left parentheses
+right parenthesis is encountered
+Step 3: Pop operand 2 from operand stack
+Step 4: Pop operand 1 from operand stack
+Step 5: Pop operator from operator stack
+Step 6: Concatenate operator and operand 1
+Step 7: Concatenate result with operand 2
+Step 8: Push result into the operand stack
+Step 9: END
