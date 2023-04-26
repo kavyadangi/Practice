27761097ef1c403d5e9ff4dab300a45a4a45dbde
Kavya <kavyadangi09@gmail.com>
2023-04-26 22:23:27 +0530

    Create circuueue.c

---
+++ newpractice.c/circuueue.c
@@ -0,0 +1,17 @@
+Step 1: IF FRONT = and Rear = MAX-1
+Write OVERFLOW
+IF FRONT=-1 and REAR=-1
+SET FRONT = REAR =
+ELSE IF REAR = MAX-1 and FRONT !=
+SET REAR =
+ELSE
+SET REAR = REAR+1
+[END OF IF]
+Step 3: SET QUEUE[REAR] = VAL
+Step 4: EXIT
+Step 2:
+" ”
+Goto step 4
+[End OF IF]
+Figure 8.19 Algorithm to insert an element in a 
+circular queue
