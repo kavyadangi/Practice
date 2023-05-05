ac02ab7af8b4968d1411d3f13ba62d7f8df7c829
Kavya <kavyadangi09@gmail.com>
2023-05-06 02:21:40 +0530

    Create trees.c

---
+++ newpractice.c/trees.c
@@ -0,0 +1,38 @@
+Step 1: Repeat Steps 2 to 4 while TREE != NULL
+Step 2: Write TREE DATA
+Step 3: PREORDER(TREE LEFT)
+Step 4: PREORDER(TREE RIGHT)
+[END OF LOOP]
+Step 5: END
+->
+->
+->
+Figure 9.16 Algorithm for pre-order traversal
+
+
+
+Step 1: Repeat Steps 2 to 4 while TREE != NULL
+Step 2: INORDER(TREE LEFT)
+Step 3: Write TREE DATA
+Step 4: INORDER(TREE RIGHT)
+[END OF LOOP]
+Step 5: END
+->
+->
+->
+Figure 9.17 Algorithm for in-order traversal
+
+
+
+
+Step 1: Repeat Steps 2 to 4 while TREE != NULL
+Step 2: POSTORDER(TREE LEFT)
+Step 3: POSTORDER(TREE RIGHT)
+Step 4: Write TREE DATA
+[END OF LOOP]
+Step 5: END
+->
+->
+->
+Figure 9.18 Algorithm for post-order traversal
+
